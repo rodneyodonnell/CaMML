4d136b7f976e925f59e8651ab0a6f2202f9ee7c0
Rodney O'Donnell <rodo@ubuntu.(none)>
2011-01-18 17:08:50 -0800

    Updated runner scripts & README

--- camml_weka.sh
+++ camml_weka.sh
@@ -3,4 +3,4 @@ CAMML_HOME=`pwd`
 for JAR in $CAMML_HOME/jar/*.jar; do JARS=$JARS:$JAR; done
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CAMML_HOME/lib
 
-java $JVM_OPTS -classpath Camml:CDMS:$JARS cdms.core.Cdms cdms.plugin.fpli.Fpli\$Interpreter Camml/script/cammlBootstrap.fp
+java $JVM_OPTS -classpath Camml:CDMS:$JARS camml.plugin.weka.CaMMLClassifier $*
